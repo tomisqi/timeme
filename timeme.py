374d690ed7d9dfaccad6177b4b918a16f450636a
Tony Fiallos <afiallosh@gmail.com>
2019-04-23 00:15:59 +0200

    Different cmd line summary; Add WkNo in summary; Activity name in summary

--- timeme.py
+++ timeme.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-"""  timeme: Utility for timing activities
-     For help use: python3 timeme -h
+"""  timeme.py: Utility for timing activities
+     For help use: python3 timeme.py -h
 
      Author: Antonio Fiallos (afiallosh@gmail.com)
 """
@@ -8,11 +8,15 @@ __version__ = '0.0'
 
 '''
   TODOs:
-  [ ] Remove the need to specify wk_act in summary
-  [ ] Show week start when presenting summary
-  [ ] Print actName in summary
-  [ ] Improve TimeToWkNumber (* 100)
-  [ ] Improve ShowData()
+  [X] Remove the need to specify wk_act in summary
+  [X] Show week start when presenting summary
+  [X] Print actName in summary
+  [X] Improve TimeToWkNumber (* 100)
+  [X] Improve ShowData()
+  [ ] Support adding activities not based on current time
+  [ ] Show week start date in summary
+  [ ] Code is assuming that actNos are continuous
+  [ ] Add support for summary based on time of day
   [ ] Readme
 '''
 
@@ -21,20 +25,30 @@ import argparse
 import yaml
 from datetime import datetime, timedelta
 
-YAML_FILE  = 'timeme_data.yaml'
+YAML_FILE = 'timeme_data.yaml'
 SUMMARY_WK_ACT = 'wk_act'
 YEAR0 = 2019
 
+def SaveToYaml(actData):
+   stream = open(YAML_FILE, 'w')
+   yaml.dump(actData, stream)
+
+def LoadYaml():
+   try:
+      stream = open(YAML_FILE, 'r')
+      actData = yaml.load(stream)
+   except:
+      print ("File \"%s\" not found. Create actvities with -add." % YAML_FILE)
+      actData = None
+   return actData
+
 def ShowData(actData):
-   print ("ActData:")
-#   print (actData)
    for actNo, act in actData.items():
-      print ('[%d]: %s:' % (actNo, act['name']), end=' '),
+      print ('[%d]: %s' % (actNo, act['name']))
       times = act['times']
       if (times):
          for i in range(len(times)):
-            print ( '%s->%s' % (times[i]['start'], times[i]['end']), end=' ')
-      print ()
+            print('start:%s  end:%s' % (times[i]['start'], times[i]['end']))
 
 def FindUnusedActNo(actData):
    result = 0
@@ -55,19 +69,6 @@ def FindActivity(actData, actNo):
       print ("Activity number \"%d\" not found." % actNo)
       sys.exit()
 
-def SaveToYaml(actData):
-   stream = open(YAML_FILE, 'w')
-   yaml.dump(actData, stream)
-
-def LoadYaml():
-   try:
-      stream = open(YAML_FILE, 'r')
-      actData = yaml.load(stream)
-   except:
-      print ("File \"%s\" not found. Create actvities with -add." % YAML_FILE)
-      actData = None
-   return actData
-
 def FindMaxMinTime(actData):
    result = {'min': datetime.max, 'max': datetime.min}
    for actNo, act in actData.items():
@@ -80,32 +81,33 @@ def FindMaxMinTime(actData):
             result['max'] = time
    return result
       
-def TimeToWkNumber(dtime):
-   yr = dtime.isocalendar()[0]
-   yrIdx = yr - YEAR0
-   wk = dtime.isocalendar()[1]
-   wkIdx = yrIdx * 100 + wk
-   return wkIdx
+def GetWkNumber(dtime):
+   yr = dtime.year
+   yrNo = yr - YEAR0
+   wk = dtime.isocalendar()[1] - 1 # Note: isocalendar()[1] gives us the week number
+   wkNo = yrNo * 53 + wk # Note: There are max 53 weeks per year
+   return wkNo
 
-def BuildSummary(actData):
-   timeRange = FindMaxMinTime(actData)
-   baseWk = TimeToWkNumber(timeRange['min'])
-   wkCount = 1 + (TimeToWkNumber(timeRange['max']) - baseWk)
-
+def BuildAndPrintAllWks(actData):
    actCount = len(actData.items())
-   
-   print("actCount=%d wkCount=%d" % (actCount, wkCount))
+
+   # Find wkCount and baseWk
+   timeRange = FindMaxMinTime(actData)
+   baseWk = GetWkNumber(timeRange['min'])
+   wkCount = 1 + (GetWkNumber(timeRange['max']) - baseWk)
    '''
-   Note: How data looks like:
+   Note: Example of how data could like:
    actData = {0: {'name': actName, 'times': [{'start': 01:00, 'end': 02:00}, {'start': 03:00, 'end': 04:00}],
               1: {'name': actName, 'times': [{'start': 01:00, 'end': 02:00}, {'start': 03:00, 'end': 04:00}]}
 
    allWks = [wk0, wk1, wk2]
    wk0 = [[00:00, 00:00, 00:00], [00:00, 00:00, 00:00], [00:00, 00:00, 00:00], [00:00, 00:00, 00:00], ...]
    '''
+   print("actCount=%d wkCount=%d" % (actCount, wkCount))
+   
    # Initialize allWks[]
    allWks = []
-   for i in range(wkCount):
+   for i in range(wkCount):  # Note: We assume that actNos in actData are continuous
       wk = []
       allWks.append(wk)
       for d in range(7):
@@ -121,31 +123,34 @@ def BuildSummary(actData):
          time = times[i]
          if (time['end']): # Only edit time in activities that have ended
             ellapsed = time['end'] - time['start']
-            wkIdx = TimeToWkNumber(time['start']) - baseWk
-            dIdx = time['start'].isocalendar()[2] - 1 # isocalendar[2] returns week day
+            wkIdx = GetWkNumber(time['start']) - baseWk
+            dIdx = time['start'].weekday()
             wk = allWks[wkIdx]
             wk[dIdx][actNo] += ellapsed
 
-   # print allWks[]
-   for i in range(wkCount):
-      PrintWk(allWks[i])
+   # Print allWks[]
+   for i in range(len(allWks)):
+      print ("Wk: %d " % (baseWk + i)) # TODO: Use week start date instead
+      PrintWk(allWks[i], actData)
 
-def PrintWk(wkData):
-   print ('     Mon     Tue     Wed     Thu     Fri     Sat     Sun')
-   for i in range(len(wkData[0])):
-      print ('[%d] ' % (i),  end=' ')
+def PrintWk(wkData, actData):
+   #TODO: we arbitrarily use 20 as length for alignment- use instead len(longest(actName))
+   print('{:<20s}{:<0s}'.format('', ' Mon     Tue     Wed     Thu     Fri     Sat     Sun'))
+   for actNo in actData.keys():
+      sAct = '[%d] %s' % (actNo, actData[actNo]['name'])
+      sTimes = ''
       for d in range(7):
-         print ('%s' % str(wkData[d][i])[:7],  end=' ')
-      print ()
-
+         sTimes += ' %s' % str(wkData[d][actNo])[:7]
+      print('{:<20s}{:<0s}'.format(sAct, sTimes))
+      
 if __name__ == '__main__':
 
    argsAll = [(('--show'), {'required': False, 'help': 'Show activities', 'action': 'store_const', 'const': True}),
+              (('--summary'), {'required': False, 'help': 'Summary of activities', 'action': 'store_const', 'const': True}),
               (('-add'), {'required': False, 'help': 'Add activity', 'metavar': 'Activity name'}),
               (('-start'), {'required': False, 'help': 'Start activity', 'metavar': 'Activity number'}),
               (('-end'), {'required': False, 'help': 'End activity', 'metavar': 'Activity number'}),
-              (('-cancel'), {'required': False, 'help': 'Cancel activity', 'metavar': 'Activity number'}),
-              (('-summary'), {'required': False, 'help': 'Summary of activities', 'choices': [SUMMARY_WK_ACT]}),]
+              (('-cancel'), {'required': False, 'help': 'Cancel activity', 'metavar': 'Activity number'}),]
    parser = argparse.ArgumentParser(description='timeme: Utility for timing activities')
    for positional_args, keyword_args in argsAll:
       parser.add_argument(positional_args, **keyword_args)
@@ -164,12 +169,12 @@ if __name__ == '__main__':
       if (args['start']):
          actNo = int(args['start'])
          act = FindActivity(actData, actNo)
-         htime = {'start': currentTime, 'end': None}
+         newtime = {'start': currentTime, 'end': None}
          times = act['times']
          if (len(times) == 0 or times[-1]['end']):
-            act['times'].append(htime) # append only if we have finished previous or is first
+            act['times'].append(newtime) # append only if we have finished previous or is first
          else:
-            times[-1] = htime
+            times[-1] = newtime
       if (args['end']):
          actNo = int(args['end'])
          act = FindActivity(actData, actNo)
@@ -192,7 +197,7 @@ if __name__ == '__main__':
             sys.exit() 
          del times[-1]
       if (args['summary']):
-         BuildSummary(actData)
+         BuildAndPrintAllWks(actData)
 
    if (args['add']):
       act = BuildActivity(args['add'])
